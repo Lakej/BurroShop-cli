f3f4b24848cb089cce6f50ebd5d8bb7d534ce835
Lakej <alikingno@gmail.com>
2019-05-05 13:56:12 -0500

    Assets nuevos

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default{
+    apiKey: "AIzaSyCuVNCvi5VALkI1o5fErYfSfr8weNyLWu0",
+    authDomain: "bshop-7a145.firebaseapp.com",
+    databaseURL: "https://bshop-7a145.firebaseio.com",
+    projectId: "bshop-7a145",
+    storageBucket: "bshop-7a145.appspot.com",
+    messagingSenderId: "787961505934",
+    appId: "1:787961505934:web:f4e56d237ac5b285"
+}
